c40b3ce5dd027a0c83bd8147bdd21de8f31bad3d
Sleepy026 <mrbrowngarden@gmail.com>
2019-05-07 15:20:46 +0200

    updated server.py with the display question route

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 import connection
+import data_handler
 
-ANSWER_KEYS = ['id','submission_time','vote_number','question_id','message','image']
-QUESTION_KEYS = ['id','submission_time','view_number','vote_number','title','message','image']
+
 
 app = Flask(__name__)
 
 @app.route('/list')
 def route_list():
     questions = connection.read_data('sample_data/question.csv')
-    return render_template('list.html', questions=questions, q_keys=QUESTION_KEYS)
+    return render_template('list.html', questions=questions, q_keys=data_handler.QUESTION_KEYS)
 
-
-
-
+@app.route('/question/<question_id>')
+def display_question(question_id):
+    displayed_question = data_handler.get_story_by_id('sample_data/question.csv', question_id)
+    return render_template('question.html', question=displayed_question)
 
 
 
