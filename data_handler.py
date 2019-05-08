04fd072aa363549deebef028bf75e7a63e271fa4
Molnar Endre <druendre@gmail.com>
2019-05-08 11:45:49 +0200

    get max id

--- data_handler.py
+++ data_handler.py
@@ -2,6 +2,7 @@ import connection
 
 ANSWER_KEYS = ['id','submission_time','vote_number','question_id','message','image']
 QUESTION_KEYS = ['id','submission_time','view_number','vote_number','title','message','image']
+MAX_ID_KEYS = ["question_max_id", "answer_max_id"]
 
 
 def get_story_by_id(filename, id):
@@ -17,3 +18,18 @@ def get_answers_by_question_id(filename, q_id):
         if data['question_id'] == q_id:
             answers.append(data)
     return answers
+
+def get_max_id(is_answer=True):
+    record = connection.read_data("/sample_data/max_id_s.csv")
+    if is_answer:
+        max_id = int(record["answer_id"])
+        max_id += 1
+        record["answer_id"] = max_id
+    else:
+        max_id = int(record["question_id"])
+        max_id += 1
+        record["question_id"] = max_id
+
+
+    connection.write_data("/sample_data/max_id_s.csv", MAX_ID_KEYS)
+    return str(max_id)
