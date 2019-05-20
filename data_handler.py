361b340706a7cc717975337c520a47e30137ba3a
meti651 <meti651@gmail.com>
2019-05-20 13:49:58 +0200

    list questions title, display question, delete question

--- data_handler.py
+++ data_handler.py
@@ -1,16 +1,49 @@
 import connection
 
 
-def get_story_by_id(filename, id):
+@connection.connection_handler
+def get_last_5_questions_title(cursor, sort_by, direction):
+    cursor.execute("""
+                    SELECT title FROM question
+                    ORDER BY %(sort_by)s %(direction)s
+                    LIMIT 5;
+                    """,
+                   {'sort_by': sort_by, 'direction': direction})
+    questions = cursor.fetchall()
+    return questions
 
 
-def get_answers_by_question_id(filename, q_id):
+@connection.connection_handler
+def get_all_questions_title(cursor, sort_by, direction):
+    cursor.execute("""
+                        SELECT title FROM question
+                        ORDER BY %(sort_by)s %(direction)s;
+                        """,
+                   {'sort_by': sort_by, 'direction': direction})
+    questions = cursor.fetchall()
+    return questions
+
+@connection.connection_handler
+def get_question_n_answers_by_question_id(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM question
+                    JOIN answer ON question.id = answer.question_id
+                    WHERE id == id;
+                    """,
+                   {'id': id})
+    questions = cursor.fetchall()
+    return questions
 
 
-def get_max_id(is_answer=True):
+@connection.connection_handler
+def delete_question(cursor, id):
+    cursor.execute("""
+                    DELETE FROM question
+                    WHERE id = %(id)s
+                    """,
+                   {'id': id})
 
 
-def sort_questions(question_list, attribute, order_direction):
 
 
 def get_question_id_by_answer_id(answer_id, filename="sample_data/answer.csv"):
